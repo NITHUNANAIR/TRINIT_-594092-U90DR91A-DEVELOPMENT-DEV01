f28b93d14ac374842473641055caf1044fbc87db
NITHUNANAIR <119724339+NITHUNANAIR@users.noreply.github.com>
2023-02-12 05:52:32 +0530

    Create payment_model.dart

---
+++ lib/src/models/payment_model.dart
@@ -0,0 +1,21 @@
+class Food{
+  final String id;
+  final String name;
+  final String imagePath;
+  final String description;
+  final String category;
+  final double amount;
+  final double donations;
+  final double visited;
+
+  Food({
+    required this.id,
+    required this.name,
+    required this.imagePath,
+    required this.description,
+    required this.category,
+    required this.amount,
+    required this.donations,
+    required this.visited
+  });
+}

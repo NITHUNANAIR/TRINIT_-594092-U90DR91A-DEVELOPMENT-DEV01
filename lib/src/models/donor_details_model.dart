796315b8c18b662367988cf69d84c21ad9837086
NITHUNANAIR <119724339+NITHUNANAIR@users.noreply.github.com>
2023-02-12 05:50:33 +0530

    Create donor_details_model.dart

---
+++ lib/src/models/donor_details_model.dart
@@ -0,0 +1,21 @@
+class UserInfo {
+  final String id;
+  final String userId;
+  final String username;
+  final String firstName;
+  final String lastName;
+  final String email;
+  final String phoneNumber;
+  final String userType;
+
+  UserInfo({
+    required this.id,
+    required this.userId,
+    required this.username,
+    required this.firstName,
+    required this.lastName,
+    required this.email,
+    required this.phoneNumber,
+    required this.userType,
+  });
+}

8c6d3a737e418e3e13540207817f754492d3340a
NITHUNANAIR <119724339+NITHUNANAIR@users.noreply.github.com>
2023-02-12 05:48:47 +0530

    Create ngo_data.dart

---
+++ lib/src/data/ngo_data.dart
@@ -0,0 +1,33 @@
+import '../models/filter_name.dart';
+
+final categories = [
+  Category(
+    numberOfNGOs: 43,
+    imagePath: "assets/images/pic1.png",
+    categoryName: "CANCER"
+  ),
+
+  Category(
+    numberOfNGOs: 23,
+    imagePath: "assets/images/pic2.png",
+    categoryName: "DISABLED"
+  ),
+
+    Category(
+    numberOfNGOs: 23,
+    imagePath: "assets/images/PIC3.png",
+    categoryName: "OLD AGE"
+  ),
+
+    Category(
+    numberOfNGOs: 90,
+    imagePath: "assets/images/PIC4.png",
+    categoryName: "INFANT"
+  ),
+
+    Category(
+    numberOfNGOS: 55,
+    imagePath: "assets/images/PIC5.png",
+    categoryName: "TRANSGENDER DEVELOPMENT CELLS"
+  ),
+];
